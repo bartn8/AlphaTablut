473c33658cd4119e75f4f1265fd29ce62c9af17b
Luca Bartolomei <bartn8@hotmail.it>
2021-05-06 22:41:38 +0200

    launch vm

--- tablut.sh
+++ tablut.sh
@@ -3,15 +3,15 @@
 if [[ $# -eq 0 ]]; then
     player="W"
     timeout=59
-    host="localhost"
+    host="127.0.0.1"
 elif [[ $# -eq 1 ]]; then
     player=$1
     timeout=59
-    host="localhost"
+    host="127.0.0.1"
 elif [[ $# -eq 2 ]]; then
     player=$1
     timeout=$2
-    host="localhost"
+    host="127.0.0.1"
 else
     player=$1
     timeout=$2
@@ -19,4 +19,5 @@ else
 fi
 
 cd "/tablut/AlphaTablut"
+python3 client.py -h
 python3 client.py -p $player -t $timeout -i $host
